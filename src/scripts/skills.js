8cd7053734ad8057f680fb54cdcae68e6b6992df
Arshaluys Aslanyan <a.aslanyan.a@gmail.com>
2020-05-19 19:03:52 +0300

    rendering titles with vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -3,4 +3,10 @@ import Vue from 'vue';
 new Vue({
     el: '#skills-component',
     template: '#skills-list',
+    data(){
+        return {
+            frontEndTitle: 'Front-end',
+            workflowTitle: 'Workflow'
+        }
+    }
 });
